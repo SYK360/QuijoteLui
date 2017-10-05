02ff16d87af5853393728ff154f8c3333b8ca1fb
Jorge <jorjoluiso@gmail.com>
2017-10-04 21:35:43 -0500

    Inicia el proyecto

---
+++ src/main/kotlin/com/quijotelui/controller/Principal.kt
@@ -0,0 +1,16 @@
+package com.quijotelui.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class Principal{
+
+    @RequestMapping("/")
+    @ResponseBody
+    fun index():String{
+        return "Bienvenido a <h3>Quijote Lui</h3> :/"
+    }
+
+}
