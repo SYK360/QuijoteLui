623880e8af22f4fabefa84d9da20d9675b94bd83
Jorge <jorjoluiso@gmail.com>
2018-01-02 09:00:08 -0500

    Cambios para generar archivo .war para Tomcat, comentados

--- src/main/kotlin/com/quijotelui/QuijoteLuiApplication.kt
+++ src/main/kotlin/com/quijotelui/QuijoteLuiApplication.kt
@@ -2,9 +2,19 @@ package com.quijotelui
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.boot.builder.SpringApplicationBuilder
+
+//Descomentar para generar archivo .war
 
 @SpringBootApplication
-class QuijoteLuiApplication
+class QuijoteLuiApplication /*: SpringBootServletInitializer()*/ {
+
+//    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+//        return application.sources(QuijoteLuiApplication::class.java!!)
+//    }
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(QuijoteLuiApplication::class.java, *args)
