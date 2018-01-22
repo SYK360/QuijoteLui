7a7290352c80124d118bfe07f1d01ae4cc115cb1
Jorge <jorjoluiso@gmail.com>
2018-01-22 17:50:33 -0500

    QuijoteLui

--- src/main/kotlin/com/quijotelui/QuijoteLuiApplication.kt
+++ src/main/kotlin/com/quijotelui/QuijoteLuiApplication.kt
@@ -3,7 +3,6 @@ package com.quijotelui
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-
 @SpringBootApplication
 class QuijoteLuiApplication
 
