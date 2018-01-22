0fad65496660ed61b17bb2b1ed0ed02235e02b0e
Jorge Luis Quiguango <jorjoluiso@gmail.com>
2018-01-22 18:32:05 -0500

    Fix view

--- src/main/kotlin/com/quijotelui/QuijoteLuiApplication.kt
+++ src/main/kotlin/com/quijotelui/QuijoteLuiApplication.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 class QuijoteLuiApplication
 
 fun main(args: Array<String>) {
-    
+
     SpringApplication.run(QuijoteLuiApplication::class.java, *args)
+    
 }
