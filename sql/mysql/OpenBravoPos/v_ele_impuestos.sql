7071e828b68fd890c25fefbbe469d29a75ea284f
Jorge Luis Quiguango <jorjoluiso@gmail.com>
2018-06-08 00:28:24 -0500

    Fix vista impuestos

--- sql/mysql/OpenBravoPos/v_ele_impuestos.sql
+++ sql/mysql/OpenBravoPos/v_ele_impuestos.sql
@@ -10,14 +10,14 @@ CREATE OR REPLACE VIEW `v_ele_impuestos` AS
         CAST(IF(((tx.RATE * 100) = 12), 2, 0) AS CHAR (10) ) AS codigo_porcentaje,
         SUM(CAST(abs((tl.UNITS * tl.PRICE)) AS DECIMAL (19 , 2 ))) AS base_imponible,
         SUM(CAST((tx.RATE * 100) AS DECIMAL (19 , 2 ))) AS tarifa,
-        SUM(CAST(abs(((tl.UNITS * tl.PRICE) * (tx.RATE + 1)))
+        SUM(CAST(if(tx.RATE = 0, 0,abs(((tl.UNITS * tl.PRICE) * (tx.RATE))))
             AS DECIMAL (19 , 2 ))) AS valor
     FROM
         ((openbravo.TICKETS t
         JOIN openbravo.TICKETLINES tl ON ((t.ID = tl.TICKET)))
         JOIN openbravo.TAXES tx ON ((tx.CATEGORY = tl.TAXID)))
 
-    GROUP BY CAST(CONCAT(t.TICKETID, tl.LINE) AS UNSIGNED) , CAST(if(t.tickettype = 0, 'FV', 'NCV') AS CHAR (10) ) , CAST(CONCAT(FUN_ESTABLECIMIENTO(),
+    GROUP BY CAST(if(t.tickettype = 0, 'FV', 'NCV') AS CHAR (10) ) , CAST(CONCAT(FUN_ESTABLECIMIENTO(),
                 FUN_PUNTO_EMISION(),
                 LPAD(t.TICKETID, 9, '0'))
         AS CHAR (20) ) , CAST('2' AS CHAR (10) ) , CAST(IF(((tx.RATE * 100) = 12), 2, 0) AS CHAR (10) );
